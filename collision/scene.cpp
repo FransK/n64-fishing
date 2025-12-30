3686c78ce9304f16d7cd515fe032007e20653546
FransK <kouwenhoven.frans@gmail.com>
2025-12-29 21:51:54 -0800

    Rudimentary collision debug text

--- collision/scene.cpp
+++ collision/scene.cpp
@@ -34,6 +34,21 @@ void Scene::update(float fixedTimeStep)
     }
 
     /* Solve collisions */
+    for (size_t i = 0; i < colliders.size(); i++)
+    {
+        for (size_t j = 0; j < colliders.size(); j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            if (Box3D::hasOverlap(colliders[i]->boundingBox, colliders[j]->boundingBox))
+            {
+                debugf("Overlapping colliders: %d and %d\n", colliders[i]->entityId, colliders[j]->entityId);
+            }
+        }
+    }
 
     /* Clamp to world */
     for (auto *c : colliders)
