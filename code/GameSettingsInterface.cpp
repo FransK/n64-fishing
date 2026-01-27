7956a342e0945bf6f247fec7d26f3ee946f8b4aa
FransK <kouwenhoven.frans@gmail.com>
2026-01-27 10:30:14 -0800

    Fix unused variable warning

--- code/GameSettingsInterface.cpp
+++ code/GameSettingsInterface.cpp
@@ -1,19 +1,19 @@
 #include "GameSettings.h"
 #include "GameSettingsInterface.h"
+#include <mutex>
 
 GameSettingsInterface *instance = nullptr;
 
 GameSettingsInterface *get_game_settings_interface()
 {
-    static bool init = []()
-    {
+    static std::once_flag initFlag;
+    std::call_once(initFlag, []()
+                   {
         if (!instance)
         {
             static GameSettings settings;
             instance = &settings;
-        }
-        return true;
-    }();
+        } });
 
     return instance;
 }
