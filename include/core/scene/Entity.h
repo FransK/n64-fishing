4b1d0e37212285b36bc60ef8464edb9b3ce1d971
FransK <kouwenhoven.frans@gmail.com>
2026-01-29 22:40:40 -0800

    Entity changes to keep track of EntityId better

--- include/core/scene/Entity.h
+++ include/core/scene/Entity.h
@@ -1,18 +1,40 @@
 #pragma once
 
+#include <utility>
 #include "Vector2.h"
 #include "Vector3.h"
-
-static int NextEntityId = 1;
 
 class Entity
 {
 public:
     Entity() : mEntityId(GetNextEntityId()) {}
+    Entity(const Entity &other)
+        : mEntityId(GetNextEntityId()), mPosition(other.mPosition), mRotation(other.mRotation) {}
+    Entity &operator=(const Entity &other)
+    {
+        mPosition = other.mPosition;
+        mRotation = other.mRotation;
+        return *this;
+    }
+    Entity(Entity &&other) noexcept
+        : mEntityId(other.mEntityId), mPosition(std::move(other.mPosition)), mRotation(std::move(other.mRotation))
+    {
+        other.mEntityId = 0;
+    }
+    Entity &operator=(Entity &&other) noexcept
+    {
+        mEntityId = other.mEntityId;
+        mPosition = std::move(other.mPosition);
+        mRotation = std::move(other.mRotation);
+        other.mEntityId = 0;
+        return *this;
+    }
+    ~Entity() = default;
 
     [[nodiscard]] Math::Vector3 getPosition() const { return mPosition; }
     [[nodiscard]] Math::Vector2 getRotation() const { return mRotation; }
     [[nodiscard]] int getEntityId() const { return mEntityId; }
+    [[nodiscard]] bool isValid() const { return mEntityId != 0; }
 
     void setPosition(const Math::Vector3 &pos) { mPosition = pos; }
     void setRotation(const Math::Vector2 &rot) { mRotation = rot; }
@@ -20,7 +42,8 @@ public:
 private:
     static int GetNextEntityId()
     {
-        return NextEntityId++;
+        static int nextId = 1;
+        return nextId++;
     }
 
     int mEntityId;
